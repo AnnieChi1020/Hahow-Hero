2056d0d4611a89a286391be6b313a093e58cd9fa
Annie Chi <elmo1419141@gmail.com>
2023-04-05 08:59:50 +0800

    feat: add test cases

--- src/pages/HeroesPage/index.test.tsx
+++ src/pages/HeroesPage/index.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import HeroesPage from './index';
-import renderWithProviders from '../../utils/test-utils';
+import { renderWithProviders } from '../../utils/test-utils';
 
 describe('HeroesPage', () => {
     it('should render correctly', () => {
